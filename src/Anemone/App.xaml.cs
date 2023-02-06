14909166d21cfa4384c4d86e16ea3f416c34334e
Łukasz Suliga <grandstais3@gmail.com>
2023-02-06 19:50:30 +0100

    add logging framework
    adds serilog as well as debug console

--- src/Anemone/App.xaml.cs
+++ src/Anemone/App.xaml.cs
@@ -1,22 +1,106 @@
-﻿using System.Windows;
-using Anemone.Views;
-using Prism.Ioc;
+﻿#if DEBUG
+#define AttachConsole
+#endif
 
-namespace Anemone
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using Anemone.Startup;
+using Anemone.Views;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Prism.Ioc;
+using Prism.Unity;
+using Serilog;
+using Serilog.Events;
+using Unity;
+using Unity.Microsoft.DependencyInjection;
+
+namespace Anemone;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     a queue of logging data used for storing logging messages before logger is created
     /// </summary>
-    public partial class App
+    private List<LoggingQueueItem>? _loggerQueue = new();
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
     {
-        protected override void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-        }
-
-        protected override Window CreateShell()
-        {
-            var w = Container.Resolve<ShellView>();
-            return w;
-        }
     }
+
+    protected override Window CreateShell()
+    {
+        Log.Logger.Debug("creating shell");
+        var w = Container.Resolve<ShellView>();
+        return w;
+    }
+        
+    protected override IContainerExtension CreateContainerExtension()
+    {
+        var container = new UnityContainer();
+        IServiceCollection serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+        container.BuildServiceProvider(serviceCollection);
+        return new UnityContainerExtension(container);
+    }
+        
+    private static IConfiguration CreateConfiguration()
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("secrets.json", true, true)
+            .AddUserSecrets<App>()
+            .Build();
+        return configuration;
+    }
+        
+    private void ConfigureLogger(IConfiguration configuration)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+
+        ArgumentNullException.ThrowIfNull(_loggerQueue);
+        foreach (var item in _loggerQueue)
+            // ReSharper disable once TemplateIsNotCompileTimeConstantProblem
+            Log.Logger.Write(item.LogEventLevel, item.Exception, item.Message);
+
+        _loggerQueue = null;
+    }
+        
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _loggerQueue!.Add(new LoggingQueueItem
+            { LogEventLevel = LogEventLevel.Information, Message = "starting application" });
+            
+        AllocConsole();
+        var configuration = CreateConfiguration();
+        ConfigureLogger(configuration);
+
+        base.OnStartup(e);
+    }
+        
+    protected override void OnExit(ExitEventArgs e)
+    {
+        Log.Logger.Information("stopping application");
+        FreeConsole();
+        base.OnExit(e);
+    }
+    
+    [DllImport("Kernel32")]
+    [Conditional("AttachConsole")]
+    private static extern void AllocConsole();
+
+    [DllImport("Kernel32")]
+    [Conditional("AttachConsole")]
+    private static extern void FreeConsole();
 }
+    
---
+++ src/Anemone/Startup/LoggingQueue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Serilog.Events;
+
+namespace Anemone.Startup;
+
+internal class LoggingQueueItem
+{
+    public required LogEventLevel LogEventLevel { get; set; }
+    public required  string Message  { get; set; }
+    public Exception? Exception  { get; set; }
+}
