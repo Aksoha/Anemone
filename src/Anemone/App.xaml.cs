205e1e46dc553de77ecb883644ed851fe4a1e3d2
Łukasz Suliga <grandstais3@gmail.com>
2023-02-06 18:30:10 +0100

    create prism application

---
+++ src/Anemone/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Anemone.Views;
+using Prism.Ioc;
+
+namespace Anemone
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override Window CreateShell()
+        {
+            var w = Container.Resolve<ShellView>();
+            return w;
+        }
+    }
+}
