7bbc53503f0663e22d9c72067581bdc15047ecf1
Łukasz Suliga <grandstais3@gmail.com>
2023-03-07 11:45:15 +0100

    add async command

---
+++ src/Anemone.Core/ActionCommandAsync.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Anemone.Core;
+
+public class ActionCommandAsync : ICommand
+{
+    private readonly Func<bool>? _canExecuteHandler;
+    private readonly Func<Task> _executedHandler;
+
+    public ActionCommandAsync(Func<Task> executedHandler, Func<bool>? canExecuteHandler = null)
+    {
+        _executedHandler = executedHandler;
+        _canExecuteHandler = canExecuteHandler;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecuteHandler == null || _canExecuteHandler();
+    }
+
+    public void Execute(object? parameter)
+    {
+        _executedHandler();
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
