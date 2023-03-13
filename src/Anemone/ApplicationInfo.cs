f5b3a86773c7f5279dddcd49676553cc3d37fe17
Łukasz Suliga <grandstais3@gmail.com>
2023-03-13 14:18:59 +0100

    add application info class

---
+++ src/Anemone/ApplicationInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Anemone;
+
+public static class ApplicationInfo
+{
+    private const string DatabaseName = "data.db";
+
+    public static readonly string LocalFilesDirectory = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+        Assembly.GetEntryAssembly()!.GetName()!.Name!);
+
+    private static readonly string DatabasePath = Path.Combine(LocalFilesDirectory, DatabaseName);
+
+    public static readonly string ConnectionString = @$"Data Source={DatabasePath};";
+}
