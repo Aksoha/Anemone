e3a77f905a746da1deb9b92cb025dcaf3326d5da
Łukasz Suliga <grandstais3@gmail.com>
2023-03-29 17:02:19 +0200

    add search box

--- src/Anemone.Algorithms/ViewModels/HeatingRepositoryListViewModel.cs
+++ src/Anemone.Algorithms/ViewModels/HeatingRepositoryListViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Anemone.Algorithms.Models;
@@ -16,9 +18,33 @@ namespace Anemone.Algorithms.ViewModels;
 public class HeatingRepositoryListViewModel : ViewModelBase
 {
     private HeatingSystemListName? _selectedItem;
+    private string? _searchString;
+    private IEnumerable<HeatingSystemListName> _filteredItems = null!;
+    private bool _isAscendingOrder = true;
+
+    public bool IsAscendingOrder
+    {
+        get => _isAscendingOrder;
+        set
+        {
+            SetProperty(ref _isAscendingOrder, value);
+            OrderItems();
+        }
+    }
+
+    private void OrderItems()
+    {
+        if (IsAscendingOrder)
+        {
+            FilteredItems = FilteredItems.OrderBy(x => x.Name);
+            return;
+        }
+        FilteredItems = FilteredItems.OrderByDescending(x => x.Name);
+    }
 
     public HeatingRepositoryListViewModel(ILogger<HeatingRepositoryListViewModel> logger,
-        IHeatingSystemRepository repository, IToastService toastService, IDialogService dialogService, IEventAggregator eventAggregator)
+        IHeatingSystemRepository repository, IToastService toastService, IDialogService dialogService,
+        IEventAggregator eventAggregator)
     {
         Logger = logger;
         Repository = repository;
@@ -30,6 +56,9 @@ public class HeatingRepositoryListViewModel : ViewModelBase
         DeleteCommand = new ActionCommandAsync(ExecuteDeleteCommand);
         FetchDataCommand.Execute(null);
         UpdateDataSource = ExecuteFetchDataCommand;
+        FilteredItems = ItemsSource;
+
+        ItemsSource.CollectionChanged += (_, _) => RaisePropertyChanged(nameof(IsRepositoryListVisible));
     }
 
 
@@ -41,7 +70,28 @@ public class HeatingRepositoryListViewModel : ViewModelBase
     public ICommand FetchDataCommand { get; set; }
     public ICommand RenameCommand { get; set; }
     public ICommand DeleteCommand { get; set; }
+
+    public string? SearchString
+    {
+        get => _searchString;
+        set
+        {
+            if (SetProperty(ref _searchString, value))
+                FilterItems();
+        }
+    }
+
+    
+    public IEnumerable<HeatingSystemListName> FilteredItems
+    {
+        get => _filteredItems;
+        set => SetProperty(ref _filteredItems, value);
+    }
+
     public Func<Task> UpdateDataSource { get; }
+
+    // public bool IsRepositoryListVisible => false;
+    public bool IsRepositoryListVisible => ItemsSource.Count > 0;
 
     /// <summary>
     ///     Item on which <see cref="RenameCommand" /> and <see cref="DeleteCommand" /> will be performed.
@@ -61,12 +111,15 @@ public class HeatingRepositoryListViewModel : ViewModelBase
         EventAggregator.GetEvent<HeatingSystemSelectionChangedEvent>().Publish(value);
     }
 
-    public ObservableCollection<HeatingSystemListName> ItemsSource { get; set; } = new();
+    public ObservableCollection<HeatingSystemListName> ItemsSource { get; } = new();
 
     public async Task<HeatingSystem?> Get(HeatingSystemListName item)
     {
         var result = await Repository.Get(item.Id);
-        return result is null ? null : new HeatingSystem {Id = (int)result.Id!, Name = result.Name, Points = result.HeatingSystemPoints.ToArray()};
+        return result is null
+            ? null
+            : new HeatingSystem
+                { Id = (int)result.Id!, Name = result.Name, Points = result.HeatingSystemPoints.ToArray() };
     }
 
     private async Task ExecuteFetchDataCommand()
@@ -81,7 +134,7 @@ public class HeatingRepositoryListViewModel : ViewModelBase
         ArgumentNullException.ThrowIfNull(SelectedItem);
 
 
-        var data = await Repository.Get(SelectedItem.Id!);
+        var data = await Repository.Get(SelectedItem.Id);
         if (data is null)
         {
             OnItemNotFoundInRepository(SelectedItem);
@@ -122,6 +175,12 @@ public class HeatingRepositoryListViewModel : ViewModelBase
         await Repository.Delete(data);
         ItemsSource.Remove(SelectedItem);
         SelectedItem = null;
+    }
+
+    private void FilterItems()
+    {
+        FilteredItems = ItemsSource.Where(x =>
+            x.Name.StartsWith(SearchString?.Trim() ?? string.Empty, StringComparison.OrdinalIgnoreCase));
     }
 
     private void OnItemNotFoundInRepository(HeatingSystemListName item)
